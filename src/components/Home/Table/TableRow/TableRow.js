a0edabd54550f7e27ce14e6c06a1edb882f95a05
Lai Hieu <laihieu2714@gmail.com>
2024-10-22 20:11:17 +0700

    Refactor TableRow component to add checkbox for task items and update progress bar style

--- src/components/Home/Table/TableRow/TableRow.js
+++ src/components/Home/Table/TableRow/TableRow.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import ChildProjectButton from "../ChildProjectButton/ChildProjectButton";
 import EditButton from "../EditButton/EditButton";
 import DeleteButton from "../DeleteButton/DeleteButton";
 import style from "./TableRow.module.css"; // Chúng ta sẽ tạo file CSS riêng cho ProjectItem
 import Manager from "../ManagerButton";
-
+import { API_BASE_URL } from "../../Table/api";
 
 function formatDatetimeForInput(datetimeString) {
   if (datetimeString === null) {
@@ -30,19 +31,49 @@ function ProjectItem({
   setHistory,
   setReloadTableData,
 }) {
+  const [isFinished, setIsFinished] = useState(null);
+
+  const handleCheckboxChange = async () => {
+    const newFinishedState = !isFinished;
+    setIsFinished(newFinishedState);
+    await handleFinishProject(newFinishedState);
+  };
+
+  useEffect(() => {
+    setIsFinished(item.progress === 100);
+  }, [item.progress]);
+
+  const handleFinishProject = async () => {
+    const url = `${API_BASE_URL}/project/${item.id}/`;
+    const response = await fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        progress: isFinished ? 0 : 100,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    setReloadTableData((prev) => !prev);
+  };
+
   if (isEditing && editingValue.id === item.id) {
     item = editingValue;
     item.isDisabled = false;
     item.isEditing = true;
   }
+
   return (
     <tr className={style.projectRow}>
       <td className={style.projectCell}>
         <ChildProjectButton item={item} setHistory={setHistory} />
       </td>
-        {/* <td className={style.projectCell}>
-          {item.type === "task" ? <MdOutlineTask /> : <GrTask />}
-        </td> */}
       <td className={style.projectCell}>
         <input
           value={item.title}
@@ -54,7 +85,7 @@ function ProjectItem({
       </td>
       <td className={style.projectCell}>
         <textarea
-          rows={2}
+          rows={1}
           value={item.description}
           disabled={item.isDisabled}
           onChange={handleEditValue}
@@ -80,14 +111,27 @@ function ProjectItem({
           name="time_end"
         />
       </td>
-
       <td className={style.projectCell}>
-        <div className={style.progressContainer}>
-          <div
-            className={style.progressBar}
-            style={{ width: "10%", height: "20px" }}
-          ></div>
-        </div>
+        {item.type === "project" ? (
+          <div className={style.progressContainer}>
+            <div
+              className={style.progressBar}
+              style={{ width: `${item.progress}%`, height: "20px" }}
+            >
+              {item.progress}%
+            </div>
+          </div>
+        ) : (
+          <label className={style.customCheckbox}>
+            <input
+              type="checkbox"
+              className={style.hiddenCheckbox}
+              checked={isFinished}
+              onChange={handleCheckboxChange}
+            />
+            <span className={style.customCheckbox}></span>
+          </label>
+        )}
       </td>
       <td>
         <Manager item={item} />
